b3447576b4bc439bd13a4076b916665cac3ee98f
Jouni Roivas <jroivas@iki.fi>
2014-10-26 11:53:35 +0200

    Completing CMake
    Change-Id: Ida0c5773228f827993bbc5dc13d00739fe58bc8f
    Reviewed-on: https://blistud.com/gerrit/386
    Reviewed-by: Jouni Roivas

--- tools/fetch_build.sh
+++ tools/fetch_build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ -d "tools/build" ] ; then
-	cd "tools/build" && git pull
-	exit 0
+    cd "tools/build" && git pull
+    exit 0
 fi
 url="$1"
 if [ "x$url" == "x" ] ; then
-	url="git@blistud.com:/opt/git/build-tools.git"
+    url="git@blistud.com:/opt/git/build-tools.git"
 fi
 git clone "$url" tools/build
 if [ ! -d "tools/build" ] ; then
-	echo "Usage: $0 build-tools.git"
-	exit 1
+    echo "Usage: $0 build-tools.git"
+    exit 1
 fi
