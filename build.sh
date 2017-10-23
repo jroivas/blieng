c3ed06cfc711d50e0690bf7680900577d6dba917
Jouni Roivas <jroivas@iki.fi>
2017-10-23 22:57:16 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ set -u
 
 if [ $# -lt 1 ] ; then
     echo "Usage: $0 SRC_DIR"
+    exit 2
 fi
 
 cd "$1"
 mkdir -p build-2
 cd build-2
-meson.py "$1" || true
+
+MESON=$(which meson.py || which meson)
+"${MESON}" "$1" || true
 ninja
